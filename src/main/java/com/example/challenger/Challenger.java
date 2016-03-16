3b42a72f21bf1c9b620df2162947aacd210a307d
Marine <marine.lacourie@gmail.com>
2016-03-16 18:50:17 +0800

    set up the project and Selenium dependency

---
+++ src/main/java/com/example/challenger/Challenger.java
@@ -0,0 +1,12 @@
+package com.example.challenger;
+
+/**
+ * Created by marin on 16/03/2016.
+ */
+public class Challenger {
+    public static void main (String [] Args) {
+
+
+
+    }
+}
---
+++ src/test/java/com/example/challenger/FirstTest.java
@@ -0,0 +1,17 @@
+package com.example.challenger;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.WebDriver;
+import static junit.framework.Assert.assertTrue;
+
+public class FirstTest {
+
+    public void testDriver() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://www.challenger.com.sg");
+        assertTrue(driver.getTitle().contentEquals("Challenger Singapore"));
+    }
+}
+
+//HOW DO I KNOW IT PASSED?
