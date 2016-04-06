aadc42b8f5149230ed7dc8836d026013b835f56b
Marine <marine.lacourie@gmail.com>
2016-04-06 22:03:02 +0800

    Add .feature files

--- src/test/java/com/example/challenger/LoginSteps.java
+++ src/test/java/com/example/challenger/LoginSteps.java
@@ -1,13 +1,15 @@
 package com.example.challenger;
 
 import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.WebDriver;
 import static junit.framework.Assert.assertTrue;
+import cucumber.api.java.en.Given;
 
-public class FirstTest {
+public class LoginSteps {
 
-    public void testDriver() {
+
+    @Given("I am on the homepage of the website")
+    public void I_am_on_the_homepage_of_the_website() {
         WebDriver driver = new ChromeDriver();
         driver.get("http://www.challenger.com.sg");
         assertTrue(driver.getTitle().contentEquals("Challenger Singapore"));
